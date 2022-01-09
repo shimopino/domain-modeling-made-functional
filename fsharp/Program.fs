19d40fc64928d9fb239fe9d2fd076a10eb6dc615
shimopino <shimoke4869@gmail.com>
2022-01-09 18:27:24 +0900

    feat: サンプル実装を追加

--- fsharp/Program.fs
+++ fsharp/Program.fs
@@ -1,4 +1,8 @@
-﻿
-
-// For more information see https://aka.ms/fsharp-console-apps
+﻿// For more information see https://aka.ms/fsharp-console-apps
 printfn "Hello from F#"
+
+let add x = x + 1
+
+type Person = { First: string; Last: string }
+
+let aPerson = { First = "Alex"; Last = "Admas" }
---
+++ fsharp/src/chapter4/UnderstaingFunctions.fs
@@ -0,0 +1,11 @@
+let add1 x = x + 1
+
+let add x y = x + y
+
+let squarePlusOne x =
+    let square = x * x
+    square + 1
+
+let areEqual x y = (x = y)
+
+printfn add1 1
