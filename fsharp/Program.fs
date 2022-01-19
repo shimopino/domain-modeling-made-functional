f77c6bc103393a98624ea480e2af87687ddf1600
shimopino <shimoke4869@gmail.com>
2022-01-20 06:00:10 +0900

    feat: TypSAcriptでの再現を始める

--- fsharp/Program.fs
+++
@@ -1,8 +0,0 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
-
-let add x = x + 1
-
-type Person = { First: string; Last: string }
-
-let aPerson = { First = "Alex"; Last = "Admas" }
