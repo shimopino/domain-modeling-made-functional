5621856915ba68cd629191c4c375018d4808964d
shimopino <33191285+shimopino@users.noreply.github.com>
2022-09-25 14:11:14 +0000

    Result型からクラスを除去

--- result/Result.test.ts
+++ result/Result.test.ts
@@ -1,11 +1,13 @@
 import { expect, test } from 'vitest';
-import { bind, err, mapError, ok, Result } from './Result';
-import { pipe } from './pipe';
+import { err, ok, Result } from './Result';
 
-class NameEmptyError extends Error {}
+const NameEmptyError = {
+  type: 'NameEmptyError',
+} as const;
+type NameEmptyError = typeof NameEmptyError;
 
-const resultHello = (name: string): Result<string, NameEmptyError> => {
-  if (name === '') return err(new NameEmptyError());
+const resultHello = (name: string): Result<string, typeof NameEmptyError> => {
+  if (name === '') return err(NameEmptyError);
 
   return ok(`hello ${name}`);
 };
@@ -26,51 +28,6 @@ test('err関数', () => {
   expect(result.ok).toBe(false);
 
   if (!result.ok) {
-    expect(result.error).toStrictEqual(new NameEmptyError());
+    expect(result.error).toStrictEqual(NameEmptyError);
   }
-});
-
-test('[成功ケース] bind関数で1ウェイ関数を2ウェイ関数に変換する', () => {
-  const twoTrack = bind(resultHello);
-
-  const okResult = ok(`shimokawa`);
-  const result = twoTrack(okResult);
-
-  expect(result.ok).toBe(true);
-  if (result.ok) expect(result.value).toBe('hello shimokawa');
-});
-
-test('[失敗ケース] bind関数で1ウェイ関数を2ウェイ関数に変換する', () => {
-  const twoTrack = bind(resultHello);
-
-  const errResult = err(new NameEmptyError());
-  const result = twoTrack(errResult);
-
-  expect(result.ok).toBe(false);
-  if (!result.ok) {
-    expect(result.error).toStrictEqual(new NameEmptyError());
-  }
-});
-
-class NotNegativeValueError extends Error {}
-class Over100Error extends Error {}
-type ValidationError = NotNegativeValueError | Over100Error;
-
-const add10 = (value: number): Result<number, NotNegativeValueError> => {
-  if (value < 0) {
-    return err(new NotNegativeValueError());
-  }
-  return ok(value + 10);
-};
-
-const tellNumber = (value: number): Result<string, Over100Error> => {
-  if (value > 100) {
-    return err(new Over100Error());
-  }
-
-  return ok(`hello ${value}`);
-};
-
-test('mapErrorで共通のエラー型に変換する', () => {
-    mapError()
 });
--- result/Result.ts
+++ result/Result.ts
@@ -1,6 +1,7 @@
+export type BaseError = { type: string };
 export type Ok<T> = { ok: true; value: T };
-export type Err<E> = { ok: false; error: E };
-export type Result<T, E> = Ok<T> | Err<E>;
+export type Err<E extends BaseError> = { ok: false; error: E };
+export type Result<T, E extends BaseError> = Ok<T> | Err<E>;
 
 export const ok = <T>(value: T): Ok<T> => {
   return {
@@ -9,31 +10,9 @@ export const ok = <T>(value: T): Ok<T> => {
   };
 };
 
-export const err = <E>(error: E): Err<E> => {
+export const err = <E extends BaseError>(error: E): Err<E> => {
   return {
     ok: false,
     error,
   };
 };
-
-type ResultFunc<T, E> = (args: any) => Result<T, E>;
-
-export const bind =
-  <T, E>(cb: ResultFunc<T, E>) =>
-  (result: Result<T, E>) => {
-    if (result.ok) {
-      return cb(result.value);
-    } else {
-      return err(result.error);
-    }
-  };
-
-export const mapError =
-  <T, E>(cb: ResultFunc<T, E>) =>
-  (result: Result<T, E>) => {
-    if (result.ok) {
-      return ok(result.value);
-    } else {
-      return err(cb(result.error));
-    }
-  };
