06125af7e00a7f86553b54df692c15580dd84c47
shimopino <33191285+shimopino@users.noreply.github.com>
2022-09-25 16:53:38 +0000

    型エラーは発生するが、ワークフローのサンプルを追加

---
+++ result/examples/workflow.test.ts
@@ -0,0 +1,42 @@
+import { expect, test } from 'vitest';
+import { err, ok, Result } from '../Result';
+import { pipeWith } from '../pipeWith';
+import { mapError } from '../mapError';
+import { bind } from '../bind';
+
+type NonNegativeError = { type: 'NonNegativeError' };
+type Over20Error = { type: 'Over20Error' };
+type ValidationError = NonNegativeError | Over20Error;
+
+const toValidationError = (input: unknown) => input as ValidationError;
+
+const add10 = (input: number): Result<number, NonNegativeError> => {
+  if (input < 0) {
+    return err({ type: 'NonNegativeError' });
+  }
+
+  return ok(input + 10);
+};
+
+const numberToString = (input: number): Result<string, Over20Error> => {
+  if (input > 20) {
+    return err({ type: 'Over20Error' });
+  }
+
+  return ok(String(input));
+};
+
+test('関数を結合させていく', () => {
+  const result = pipeWith(
+    10,
+    add10,
+    // @ts-expect-error TODO 厳密な型エラーでは怒られる。個別のエラーからUnion型に代入できない
+    mapError(toValidationError),
+    bind(numberToString),
+  );
+
+  expect(result.ok).toBe(true);
+  if (result.ok) {
+    expect(result.value).toBe('20');
+  }
+});
