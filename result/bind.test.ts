37c8bcc7af83e084bc5b133908b7376b65fbcd95
shimopino <33191285+shimopino@users.noreply.github.com>
2022-09-25 14:13:37 +0000

    bind関数を使用して関数を変換する

---
+++ result/bind.test.ts
@@ -0,0 +1,38 @@
+import { expect, test } from 'vitest';
+import { bind } from './bind';
+import { err, ok, Result } from './Result';
+
+const EmptyStringError = {
+  type: 'EmptyStringError',
+} as const;
+type EmptyStringError = typeof EmptyStringError;
+
+const hello = (name: string): Result<string, EmptyStringError> => {
+  if (name === '') {
+    return err(EmptyStringError);
+  }
+
+  return ok(`hello ${name}`);
+};
+
+test('[成功ケース] bind関数で、1ウェイトラック関数を2ウェイトラック関数に変換する', () => {
+  const twoTrackHello = bind(hello);
+
+  const input = ok('shimokawa');
+  const result = twoTrackHello(input);
+
+  expect(result.ok).toBe(true);
+  if (result.ok) expect(result.value).toBe('hello shimokawa');
+});
+
+test('[失敗ケース] bind関数で、1ウェイトラック関数を2ウェイトラック関数に変換する', () => {
+  const twoTrackHello = bind(hello);
+
+  const input = ok('');
+  const result = twoTrackHello(input);
+
+  expect(result.ok).toBe(false);
+  if (!result.ok) {
+    expect(result.error).toEqual({ type: 'EmptyStringError' });
+  }
+});
---
+++ result/bind.ts
@@ -0,0 +1,24 @@
+import { Result, BaseError, err } from './Result';
+import { Transform } from './functions';
+
+export type OneWayTrackFn<T, U, E extends BaseError> = Transform<
+  T,
+  Result<U, E>
+>;
+
+/**
+ * 1-wayトラック関数を、2-wayトラック関数に変換する
+ *
+ * Result型ではない値を引数に受け取り、Result型を返す関数を
+ * Result型を引数で受け取ることができる形式の関数に変換する
+ *
+ * @param fn 引数T を Result<U, E> に変換する関数
+ * @returns 引数を Result<T, E> を受け入れることができる関数
+ */
+export const bind =
+  <T, U, E extends BaseError>(fn: OneWayTrackFn<T, U, E>) =>
+  (input: Result<T, E>) => {
+    if (!input.ok) return err(input.error);
+
+    return fn(input.value);
+  };
