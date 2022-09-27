cd7af9fd1d80e719ebe781c74a243e1c380158f3
shimopino <33191285+shimopino@users.noreply.github.com>
2022-09-27 12:38:53 +0000

    map関数のエラーケースの処理を追加

--- neverthrow/examples/map.test.ts
+++ neverthrow/examples/map.test.ts
@@ -21,3 +21,32 @@ test('[成功ケース] map関数を使用して、 T=>U の関数を T=>Result<
   expect(newResult.isOk()).toBe(true);
   expect(newResult.unwrapOr('never')).toEqual({ output: 'hello shimokawa' });
 });
+
+test('[失敗ケース] map関数を使用して、 T=>U の関数を T=>Result<U,E> に変換する', () => {
+  const result = hello('');
+
+  const newResult = result.map((input: string) => ({ output: input }));
+
+  expect(newResult.isErr()).toBe(true);
+  expect(newResult.unwrapOr('never')).toEqual('never');
+  if (newResult.isErr()) {
+    expect(newResult.error).toEqual({ type: 'NonEmptyError' });
+  }
+});
+
+test('[失敗ケース] mapErr関数を使用して、 E => Result<T, F> というエラー変換を行う', () => {
+  const result = hello('');
+
+  const newResult = result
+    .map((input: string) => ({ output: input }))
+    .mapErr((error) => ({ type: 'WrapError', origin: error.type }));
+
+  expect(newResult.isErr()).toBe(true);
+  expect(newResult.unwrapOr('never')).toEqual('never');
+  if (newResult.isErr()) {
+    expect(newResult.error).toEqual({
+      type: 'WrapError',
+      origin: 'NonEmptyError',
+    });
+  }
+});
